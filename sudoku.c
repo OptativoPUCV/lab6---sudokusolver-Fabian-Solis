f3d06676922f01ef43c35df52e60b6d2b3cc334f
Replit user <>
2023-05-07 03:57:12 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -68,8 +68,8 @@ int is_valid(Node* n)
 
       if(n->sudo[c][f] != 0) 
       {
-        if(arrayCol[n->sudo[f][c]] == 1) return 0;
-        else arrayCol[n->sudo[f][c]] = 1;
+        if(arrayCol[n->sudo[c][f]] == 1) return 0;
+        else arrayCol[n->sudo[c][f]] = 1;
       }
       
       int k = 3*(f/3) + c/3;
